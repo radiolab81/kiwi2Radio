8192ebaf2c801aa9d0da42fdffce62ff6268ad57
radiolab <48355256+radiolab81@users.noreply.github.com>
2025-05-13 12:51:53 +0200

    Update kiwiinternetdb_new.py

--- kiwiinternetdb_new.py
+++ kiwiinternetdb_new.py
@@ -4,16 +4,19 @@ import requests
 import justext
 
 response = requests.get("http://kiwisdr.com/public/")
+if (len(response.text) < 1000):
+   response = requests.get("http://kiwisdr.com/.public/")  
 if response.status_code == 200:
 	paragraphs = justext.justext(response.content, justext.get_stoplist("English"))
 	fd = open("kiwi.list", "w")
 	for paragraph in paragraphs:
-		if paragraph['text'].find("http://") != -1 :
-			host = paragraph['text'][paragraph['text'].find("http://"):]
+		if paragraph.text.find("http://") != -1 :
+			#host = paragraph['text'][paragraph['text'].find("http://"):]
+			host = paragraph.text[paragraph.text.find("http://"):]
 			host = host[7:host.rfind(':')]
 			#print("host is:", host)
 
-			port = paragraph['text'][paragraph['text'].find(host):]
+			port = paragraph.text[paragraph.text.find(host):]
 			port = port[len(host)+1:port.find(" ")]
 			#print("port is:", port)
 
@@ -21,10 +24,11 @@ if response.status_code == 200:
 			#print("passwd is:", passwd)
 			
 			#desc = paragraph['text'][0:paragraph['text'].find("http://")]
-			desc = paragraph['text'][paragraph['text'].find("KiwiSDR"):]
+			desc = paragraph.text[paragraph.text.find("KiwiSDR"):]
 			#print("desc is:" , desc)
 			string_to_write = ""
 			string_to_write = host + ";" + port + ";" + passwd + ";" + desc + "\n";
+			print(string_to_write)
 			fd.write(string_to_write);
 	fd.close()
 			
